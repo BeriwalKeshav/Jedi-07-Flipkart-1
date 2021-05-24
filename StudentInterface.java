d7de8153a197967ff47537872ba29399ccb48311
kunal.com <kunalagarwal@kunal.com>
2021-05-24 19:31:04 +0530

    student interface updated

---
+++ StudentInterface.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.flipkart.service;
+
+import java.util.UUID;
+
+/**
+ * @author vibhorkansal
+ *
+ */
+public interface StudentInterface {
+	
+	
+	
+	/**
+	 * @param uname
+	 * @param uID
+	 * @param uPwd
+	 * @param sBranch
+	 * @param sRollNo
+	 * @return
+	 */
+	public int register(String uname,UUID uID,String uPwd,String sBranch,int sRollNo);
+	
+	/**
+	 * @param uId
+	 * @return
+	 */
+	public int getStudentId(UUID uId);
+	
+    	
+	/**
+	 * @param sId
+	 * @return
+	 */
+	public boolean isApproved(int sId);
+}
