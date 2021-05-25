68a69b7f85275a44779088dab765655d36b71f8c
BeriwalKeshav <keshavberiwal@Keshavs-MacBook-Pro.local>
2021-05-25 14:27:44 +0530

    Changes in Admin Interface

--- CRS-JEDI-Flipkart-Code/src/com/flipkart/service/AdminInterface.java
+++ CRS-JEDI-Flipkart-Code/src/com/flipkart/service/AdminInterface.java
@@ -9,8 +9,13 @@ import java.util.UUID;
 import com.flipkart.bean.Course;
 import com.flipkart.bean.Professor;
 import com.flipkart.bean.Student;
+import com.flipkart.exception.CourseAlreadyInCatalogException;
+import com.flipkart.exception.CourseNotOfferedException;
+import com.flipkart.exception.CourseNotRemovedException;
 import com.flipkart.exception.ProfessorAdditionFailedException;
+import com.flipkart.exception.StudentNotFoundForApprovalException;
 import com.flipkart.exception.UserNameAlreadyInUseException;
+import com.flipkart.exception.UserNotFoundException;
 
 /**
  * @author keshavberiwal
@@ -27,25 +32,25 @@ public interface AdminInterface {
 	 * @param sRollNo
 	 * @param studentList
 	 */
-	public void approveStudents(int sRollNo, List<Student> studentList);
+	public void approveStudents(int sRollNo, List<Student> studentList) throws StudentNotFoundForApprovalException;
 	
 	/**
 	 * @param course
 	 * @param courseList
 	 */
-	public void addCourse(Course course, List<Course> courseList);
+	public void addCourse(Course course, List<Course> courseList) throws CourseAlreadyInCatalogException;
 	
 	/**
 	 * @param courseCode
 	 * @param courseList
 	 */
-	public void deleteCourse(String cCode, List<Course> courseList);
+	public void deleteCourse(String cCode, List<Course> courseList)throws CourseNotRemovedException,CourseNotOfferedException;
 	
 	/**
 	 * @param cCode
 	 * @param professorId
 	 */
-	public void assignProfessor(String cCode, UUID professorId);
+	public void assignProfessor(String cCode, String professorId) throws CourseNotOfferedException,UserNotFoundException;
 	
 	/**
 	 * @param catalogId
